a59e19fd8e5b62c76a66811c9c2d132e2c1b7bb2
AlekseySamokhvalov <6560@mail.ru>
2023-05-30 03:50:04 +0300

    Инициализация.Расчеты при выводе значений в консоль каждых операции.

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,16 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.println("Задача 7.");
-        //Задача 7. Иничиализация переменных.
-        var firstFighter = 78.2;
-        var secondFighter = 82.7;
+        System.out.println("Задача 8.");
+        //Задача 8. Иничиализация переменных.
+        var workingHours = 640;
+        var employeeHours = 8;
 
-        //выод переменных в консоль
-        System.out.println("Масса первого бойца = " + firstFighter);
-        System.out.println("Масса второго бойца = " + secondFighter);
-        var massDifference = secondFighter - firstFighter;
-        System.out.println("Разница масс бойцов 1м способом = " + massDifference);
-        massDifference = secondFighter % firstFighter;
-        System.out.println("Разница масс бойцов 2м способом = " + massDifference);
+        //вывод результатов операции
+
+        System.out.println("Всего работников в компании - " + workingHours/employeeHours + " человек");
+
+        System.out.println("Если в компании работает " + (workingHours/employeeHours + 94) +
+                " человек(а), то всего " + (workingHours + employeeHours * 94) + " часа(ов) работы может быть поделено между сотрудниками");
     }
 }
