a7c51ece599f9a35a997cbe77222de58d12d2936
AlekseySamokhvalov <6560@mail.ru>
2023-05-30 03:31:28 +0300

    Инициализация, два способа расчета разницы масс бойцов. Вывод значений в консоль каждой операции.

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,17 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.println("Задача 6.");
-        //Задача 6. Иничиализация переменных.
+        System.out.println("Задача 7.");
+        //Задача 7. Иничиализация переменных.
         var firstFighter = 78.2;
         var secondFighter = 82.7;
-        var totalMass = firstFighter + secondFighter;
-        var massDifference = Math.abs(firstFighter - secondFighter);
+
         //выод переменных в консоль
         System.out.println("Масса первого бойца = " + firstFighter);
         System.out.println("Масса второго бойца = " + secondFighter);
-        System.out.println("Суммарная самма бойцов = " + totalMass);
-        System.out.println("Разница масс бойцов = " + massDifference);
+        var massDifference = secondFighter - firstFighter;
+        System.out.println("Разница масс бойцов 1м способом = " + massDifference);
+        massDifference = secondFighter % firstFighter;
+        System.out.println("Разница масс бойцов 2м способом = " + massDifference);
     }
 }
